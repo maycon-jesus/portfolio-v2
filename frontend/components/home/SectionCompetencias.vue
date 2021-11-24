26a4992854bc28fdd1f84f02b59685e8073eb767
VacuousPatter <mayconjesus07@gmail.com>
2021-11-23 22:10:14 -0300

    Linguagens de compêtencias reclassificadas

--- frontend/components/home/SectionCompetencias.vue
+++ frontend/components/home/SectionCompetencias.vue
@@ -4,20 +4,8 @@
     <div class="competencias">
       <!-- Linguagens -->
       <div class="competencia-list azul">
-        <h3>Linguagens</h3>
+        <h3>Linguagens<br>de Programação</h3>
         <ul>
-          <li>
-            <svg viewBox="0 0 24 24">
-              <path fill="currentColor" d="M12,17.56L16.07,16.43L16.62,10.33H9.38L9.2,8.3H16.8L17,6.31H7L7.56,12.32H14.45L14.22,14.9L12,15.5L9.78,14.9L9.64,13.24H7.64L7.93,16.43L12,17.56M4.07,3H19.93L18.5,19.2L12,21L5.5,19.2L4.07,3Z" />
-            </svg>
-            HTML 5: 3 Anos
-          </li>
-          <li>
-            <svg viewBox="0 0 24 24">
-              <path fill="currentColor" d="M5,3L4.35,6.34H17.94L17.5,8.5H3.92L3.26,11.83H16.85L16.09,15.64L10.61,17.45L5.86,15.64L6.19,14H2.85L2.06,18L9.91,21L18.96,18L20.16,11.97L20.4,10.76L21.94,3H5Z" />
-            </svg>
-            CSS 3: 3 Anos
-          </li>
           <li>
             <svg viewBox="0 0 24 24">
               <path fill="currentColor" d="M3,3H21V21H3V3M7.73,18.04C8.13,18.89 8.92,19.59 10.27,19.59C11.77,19.59 12.8,18.79 12.8,17.04V11.26H11.1V17C11.1,17.86 10.75,18.08 10.2,18.08C9.62,18.08 9.38,17.68 9.11,17.21L7.73,18.04M13.71,17.86C14.21,18.84 15.22,19.59 16.8,19.59C18.4,19.59 19.6,18.76 19.6,17.23C19.6,15.82 18.79,15.19 17.35,14.57L16.93,14.39C16.2,14.08 15.89,13.87 15.89,13.37C15.89,12.96 16.2,12.64 16.7,12.64C17.18,12.64 17.5,12.85 17.79,13.37L19.1,12.5C18.55,11.54 17.77,11.17 16.7,11.17C15.19,11.17 14.22,12.13 14.22,13.4C14.22,14.78 15.03,15.43 16.25,15.95L16.67,16.13C17.45,16.47 17.91,16.68 17.91,17.26C17.91,17.74 17.46,18.09 16.76,18.09C15.93,18.09 15.45,17.66 15.09,17.06L13.71,17.86Z" />
@@ -32,6 +20,26 @@
           </li>
         </ul>
       </div>
+
+      <!-- Linguagens de marcação -->
+      <div class="competencia-list ciano">
+        <h3>Linguagens<br>de Marcação</h3>
+        <ul>
+          <li>
+            <svg viewBox="0 0 24 24">
+              <path fill="currentColor" d="M12,17.56L16.07,16.43L16.62,10.33H9.38L9.2,8.3H16.8L17,6.31H7L7.56,12.32H14.45L14.22,14.9L12,15.5L9.78,14.9L9.64,13.24H7.64L7.93,16.43L12,17.56M4.07,3H19.93L18.5,19.2L12,21L5.5,19.2L4.07,3Z" />
+            </svg>
+            HTML 5: 3 Anos
+          </li>
+          <li>
+            <svg viewBox="0 0 24 24">
+              <path fill="currentColor" d="M5,3L4.35,6.34H17.94L17.5,8.5H3.92L3.26,11.83H16.85L16.09,15.64L10.61,17.45L5.86,15.64L6.19,14H2.85L2.06,18L9.91,21L18.96,18L20.16,11.97L20.4,10.76L21.94,3H5Z" />
+            </svg>
+            CSS 3: 3 Anos
+          </li>
+        </ul>
+      </div>
+
       <!-- Frameworks -->
       <div class="competencia-list roxo">
         <h3>Frameworks</h3>
@@ -121,6 +129,7 @@ section{
 
 .competencia-list{
   min-width: 300px;
+  padding: 25px;
 }
 
 .competencia-list h3{
@@ -130,6 +139,7 @@ section{
   font-size: 24px;
   font-weight: bold;
   margin-bottom: 25px;
+  line-height: 1.3;
 }
 
 .competencia-list h3::before{
@@ -146,6 +156,9 @@ section{
 }
 .competencia-list.roxo h3::before{
   background-color: #9B59B6;
+}
+.competencia-list.ciano h3::before{
+  background-color: #48dbfb;
 }
 .competencia-list.verde h3::before{
   background-color: #00B894;
@@ -177,6 +190,9 @@ section{
 }
 .competencia-list.roxo ul li svg{
   color: #9B59B6;
+}
+.competencia-list.ciano ul li svg{
+  color: #48dbfb;
 }
 .competencia-list.verde ul li svg{
   color: #00B894;
