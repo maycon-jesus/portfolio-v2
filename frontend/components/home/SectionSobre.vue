9bec862c7f6e91866edcef861b56a2dec85fac06
VacuousPatter <mayconjesus07@gmail.com>
2021-11-23 22:09:46 -0300

    Seção sobre adicionada

---
+++ frontend/components/home/SectionSobre.vue
@@ -0,0 +1,53 @@
+<template>
+  <section id="sobre" class="background-gray">
+    <Title title="Sobre Mim" />
+    <div class="container py-0">
+      <div class="imagem">
+        <img src="/images/sobre.png" alt="Foto do Maycon de corpo todo">
+      </div>
+      <div class="sobre-conteudo">
+        <p>
+          Olá! Me chamo Maycon, tenho 18 anos e gosto de criar coisas para a internet, principalmente websites.
+        </p>
+        <p>
+          Meu interesse por desenvolvimento WEB começou em 2018 quando eu precisava de um site para um chatbot do discord que eu tinha desenvolvido, e desde então eu venho me aprimorando.
+        </p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import Title from '../templates/Title.vue'
+
+export default {
+  components: { Title }
+}
+</script>
+
+<style scoped>
+section{
+  padding: 50px 25px;
+}
+
+.container{
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: center;
+}
+
+.imagem img{
+  height: 500px;
+}
+
+.sobre-conteudo{
+  max-width: 650px;
+  margin-left: 50px;
+}
+
+.sobre-conteudo p{
+  font-size: 20px;
+  margin-bottom: 20px;
+  line-height: 1.3;
+}
+</style>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <SectionApresentacao />
+    <SectionSobre />
     <SectionCompetencias />
     <SectionEducacao />
     <SectionProjetos />
@@ -10,13 +11,21 @@
 
 <script>
 import SectionApresentacao from '../components/home/SectionApresentacao.vue'
+import SectionSobre from '../components/home/SectionSobre.vue'
 import SectionCompetencias from '../components/home/SectionCompetencias.vue'
 import SectionEducacao from '../components/home/SectionEducacao.vue'
 import SectionProjetos from '../components/home/SectionProjetos.vue'
 import SectionTrabalharJuntos from '../components/home/SectionTrabalharJuntos.vue'
 
 export default {
-  components: { SectionApresentacao, SectionCompetencias, SectionEducacao, SectionProjetos, SectionTrabalharJuntos },
+  components: {
+    SectionApresentacao,
+    SectionSobre,
+    SectionCompetencias,
+    SectionEducacao,
+    SectionProjetos,
+    SectionTrabalharJuntos
+  },
 
   head: {
     title: 'Início'
