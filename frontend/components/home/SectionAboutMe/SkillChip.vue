defe9bde82ccceb90b98a1e240aebe4febc0f96f
Maycon Jesus <email@mayconjesus.dev>
2023-04-02 17:07:23 -0300

    feat: skills list improve design in mobile

--- frontend/components/home/SectionAboutMe/SkillChip.vue
+++ frontend/components/home/SectionAboutMe/SkillChip.vue
@@ -1,13 +1,35 @@
 <template>
-    <div class="skill text-body-1">
+    <div
+        class="skill"
+        :class="{
+            [props.size]: true,
+            'text-body-1': props.size === 'medium',
+            'text-body-2': props.size === 'small',
+        }"
+    >
         <slot name="default"></slot>
     </div>
 </template>
+
+<script setup lang="ts">
+import { PropType } from 'vue';
+
+const props = defineProps({
+    size: {
+        type: String as PropType<'small' | 'medium'>,
+        default: 'medium',
+    },
+});
+</script>
 
 <style lang="scss" scoped>
 .skill {
     padding: 1rem 2rem;
     border-radius: 999rem;
     background: var(--background2);
+
+    &.small {
+        padding: 0.75rem 1.5rem;
+    }
 }
 </style>
