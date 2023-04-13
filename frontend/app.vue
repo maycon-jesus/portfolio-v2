aa77d81396898d8f7e4caec906a2be9b4972aa17
Maycon Jesus <email@mayconjesus.dev>
2023-04-13 13:40:42 -0300

    fix: remove unused transition api

--- frontend/app.vue
+++ frontend/app.vue
@@ -3,3 +3,14 @@
         <NuxtPage></NuxtPage>
     </NuxtLayout>
 </template>
+
+<style>
+::view-transition-old(root) {
+    animation: none;
+    opacity: 0;
+}
+::view-transition-new(root) {
+    animation: none;
+    opacity: 1;
+}
+</style>
